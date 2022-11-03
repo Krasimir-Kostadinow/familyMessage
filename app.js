762853fab2288cc1ac28104065434d3c9f7e65a3
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-11-03 13:11:00 +0200

    new is add event listner in input

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ window.addEventListener('load', () => {
 
     const [author, content, btnSend, btnRefresh, btnDelete] = control.querySelectorAll('input');
 
+    function inputStyle(e) {
+
+        if (e.target.value.length <= 0) {
+            e.target.style.borderColor = 'red';
+        } else {
+            e.target.style.borderColor = '#ccc';
+        }
+    }
+    author.addEventListener('input', inputStyle);
+    content.addEventListener('input', inputStyle);
+
     async function sendMessage() {
 
         if (author.value !== '' && content.value !== '') {
@@ -13,28 +24,24 @@ window.addEventListener('load', () => {
                 let data = await api.requests('POST', { author: author.value, content: content.value, today: moment(new Date()).format('DD.MM.YYYY / HH.mm') });
                 author.value = '';
                 content.value = '';
-                author.style.borderColor = '#ccc';
-                content.style.borderColor = '#ccc';
-                refreshMessages();
             } catch (error) {
                 console.error(error);
-                alert(error.message);
+                alert(error.message); n
             }
         } else {
+            alert('All fields are required!');
             if (author.value === '') {
                 author.style.borderColor = 'red';
-            } else {
-                author.style.borderColor = '#ccc';
             }
-
             if (content.value === '') {
                 content.style.borderColor = 'red';
-            } else {
-                content.style.borderColor = '#ccc';
             }
-            alert('All fields are required!');
+
         }
 
+
+
+        refreshMessages();
     }
 
     async function refreshMessages() {
